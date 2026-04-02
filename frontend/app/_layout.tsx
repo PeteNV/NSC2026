0d360cff51b6dff072deb4dd7fee35db877dc098
PeteNV <pete.vimuktanan@gmail.com>
2026-04-02 18:41:38 +0700

    map theme from JSON

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,25 +1,28 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
-import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
 import { useColorScheme } from "react-native";
 import "react-native-reanimated";
 import { PaperProvider, MD3LightTheme, MD3DarkTheme } from 'react-native-paper';
+import { theme } from "../constants/theme";
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { Stack } from "expo-router";
+
 
 export const unstable_settings = {
-  anchor: "(tabs)",
+  initialRouteName: "(tabs)",
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme() ?? 'light';
 
-  const paperTheme = colorScheme === "dark" ? MD3DarkTheme : MD3LightTheme;
-  const navTheme = colorScheme === "dark" ? DarkTheme : DefaultTheme;
+  /* Theme mapped from JSON */
+  const activeTheme = theme[colorScheme];
+
+  const navTheme = colorScheme === 'dark' ? DarkTheme : DefaultTheme;
 
   return (
-    <PaperProvider theme={paperTheme}>
+    <PaperProvider theme={activeTheme}>
       <ThemeProvider value={navTheme}>
         <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         </Stack>
       </ThemeProvider>
     </PaperProvider>
