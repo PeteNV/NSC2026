a77876bed1365f4a06b0a98c15e696f5d347b12b
PeteNV <pete.vimuktanan@gmail.com>
2026-06-13 22:17:56 +0700

    feat: add liquid glass support for navbar

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -5,9 +5,29 @@ import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { NativeTabs} from 'expo-router/unstable-native-tabs';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  return (
+    <NativeTabs>
+      <NativeTabs.Trigger name="index">
+        <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="house.fill" md="home" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="explore">
+        <NativeTabs.Trigger.Icon sf="paperplane.fill" md="explore" />
+        <NativeTabs.Trigger.Label>Explore</NativeTabs.Trigger.Label>
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="scanner-test">
+        <NativeTabs.Trigger.Label>Scanner Test</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="qrcode" md="scanner" />
+      </NativeTabs.Trigger>
+    </NativeTabs>
+  );
+}
+
+/*   const colorScheme = useColorScheme();
 
   return (
     <Tabs
@@ -31,5 +51,4 @@ export default function TabLayout() {
         }}
       />
     </Tabs>
-  );
-}
+  ); */
