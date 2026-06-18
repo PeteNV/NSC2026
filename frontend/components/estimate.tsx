a047669637852785eecec8194a57ba8654a49728
IzumiWasTaken <tonkla.krubpom@gmail.com>
2026-06-18 15:56:25 +0700

    Added Usage UI without icons

---
+++ frontend/components/estimate.tsx
@@ -0,0 +1,59 @@
+import { M3Button } from '@/components/test-button';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { theme } from '@/constants/theme';
+import { StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
+
+export default function Estimate() {
+    return (
+        <ThemedView style={styles.usage}>
+          <ThemedText type="defaultSemiBold" style={{ color: theme.light.colors.onSurface }}>Usage</ThemedText>
+          <ThemedText type="title" style={{ color: theme.light.colors.onPrimaryContainer }}> ~1,234 {''}
+            <ThemedText type="subtitle" style={{ color: theme.light.colors.onPrimaryContainer }}>kWh </ThemedText>
+          </ThemedText>
+          <ThemedText type="subtitle" style={{color: theme.light.colors.secondary}}> +520 {''}
+            <ThemedText type="default" style={{ color: theme.light.colors.secondary }}>kWh from Base Estimation of 714 kWh </ThemedText>
+          </ThemedText>
+          <ThemedView style={{ backgroundColor: theme.light.colors.surfaceContainer, margin: 10, borderBottomWidth: 1, borderBottomColor: theme.light.colors.outline, marginHorizontal: 8 }} />
+        <View style={styles.rows}>
+            <View style={styles.month}>
+                <ThemedText type = "defaultSemiBold" style={{ color: theme.light.colors.secondary }}>Monthly Estimate</ThemedText>
+                <ThemedText type = "defaultSemiBold" style={{ fontSize: 18, color: theme.light.colors.onPrimaryContainer }}>~2,159 Baht</ThemedText>
+            </View>
+            <View style={styles.month}>
+                <ThemedText type = "defaultSemiBold" style={{ color: theme.light.colors.secondary }}>CO2 Footprint</ThemedText>
+                <ThemedText type = "defaultSemiBold" style={{ fontSize: 18, color: theme.light.colors.onPrimaryContainer }}>14.79 Kilograms</ThemedText>
+            </View>
+    </View>
+          <M3Button title="View Full Report" onPress={() => alert('Button pressed')}></M3Button>
+        </ThemedView>
+    );
+}
+
+const styles = StyleSheet.create({
+    usage: {
+        width: 500,
+        flex: 1,
+        justifyContent:'center',
+        padding: 20,
+        borderRadius: 15, 
+        textAlign: 'left',
+        backgroundColor: theme.light.colors.surfaceContainer,
+    },
+    month: {
+        padding: 16,
+        gap: 4,
+        justifyContent:'center',
+        textAlign: 'left',
+        backgroundColor: theme.light.colors.surfaceBright,
+        borderRadius: 15,
+        width: 200,
+    },
+    rows: {
+        flexDirection: 'row',
+        margin: 20,
+        gap: 10,
+        justifyContent:'center',
+    },
+})
