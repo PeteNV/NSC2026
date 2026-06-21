45c38382ce491c6faf4e15cd63c3ab88aa6dfced
Nubwan <149947073+Nubboyy@users.noreply.github.com>
2026-06-21 11:34:24 +0700

    feat: native-wind setup

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,22 +1,26 @@
-import { useColorScheme } from "react-native";
-import "react-native-reanimated";
-import { PaperProvider, MD3LightTheme, MD3DarkTheme } from 'react-native-paper';
-import { theme } from "../constants/theme";
-import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
-
+import { useColorScheme } from "react-native";
+import { PaperProvider } from "react-native-paper";
+import "react-native-reanimated";
+import { theme } from "../constants/theme";
+import "../style/global.css";
 
 export const unstable_settings = {
   anchor: "(tabs)",
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme() ?? 'light';
+  const colorScheme = useColorScheme() ?? "light";
 
   /* Theme mapped from JSON */
   const activeTheme = theme[colorScheme];
 
-  const navTheme = colorScheme === 'dark' ? DarkTheme : DefaultTheme;
+  const navTheme = colorScheme === "dark" ? DarkTheme : DefaultTheme;
 
   return (
     <PaperProvider theme={activeTheme}>
