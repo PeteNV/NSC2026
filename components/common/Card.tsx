a8ee4c68f27824e243fa55b5cec355bd75eecd39
Nubwan <149947073+Nubboyy@users.noreply.github.com>
2026-07-01 21:59:03 +0700

    feat: custom card components

---
+++ components/common/Card.tsx
@@ -0,0 +1,27 @@
+import { useTheme } from "@/hooks/useTheme";
+import { type StylableFC } from "@/types/common";
+import { clsx } from "clsx";
+import type { PropsWithChildren } from "react";
+import { View } from "react-native";
+
+const Card: StylableFC<PropsWithChildren> = ({
+  className,
+  style,
+  children,
+}) => {
+  const { colors } = useTheme();
+
+  return (
+    <View
+      className={clsx(className, "w-full gap-4 rounded-xl p-4")}
+      style={[
+        style,
+        { backgroundColor: colors.surface, borderColor: colors.outlineVariant },
+      ]}
+    >
+      {children}
+    </View>
+  );
+};
+
+export default Card;
