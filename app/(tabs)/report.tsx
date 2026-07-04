827935f65f8f40c97d82b94833139960ca154da9
Nubwan <149947073+Nubboyy@users.noreply.github.com>
2026-07-04 21:00:16 +0700

    feat: empty report screen

---
+++ app/(tabs)/report.tsx
@@ -0,0 +1,21 @@
+import { useTheme } from "@/hooks/useTheme";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export default function ReportScreen() {
+  const { colors } = useTheme();
+  const insets = useSafeAreaInsets();
+  return (
+    <View
+      className="flex-1 gap-4"
+      style={{
+        backgroundColor: colors.background,
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom + 16,
+      }}
+    >
+      <Text>Report Screen</Text>
+    </View>
+  );
+}
