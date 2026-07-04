f1c186ddde68a6fd48bd6ac5a2b8e97038f96b2f
PeteNV <pete.vimuktanan@gmail.com>
2026-07-04 22:03:25 +0700

    feat: add proper SF Symbols and MD Icons for tab triggers

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,18 +9,18 @@ export default function TabLayout() {
     <NativeTabs>
       <NativeTabs.Trigger name="index">
         <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon sf="house.fill" md="home" />
+        <NativeTabs.Trigger.Icon sf="house" md="home" />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="room">
-        <NativeTabs.Trigger.Icon sf="paperplane.fill" md="explore" />
+        <NativeTabs.Trigger.Icon sf="door.left.hand.open" md="door_sliding" />
         <NativeTabs.Trigger.Label>Room</NativeTabs.Trigger.Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="appliance">
-        <NativeTabs.Trigger.Icon sf="paperplane.fill" md="explore" />
+        <NativeTabs.Trigger.Icon sf="cooktop" md="microwave" />
         <NativeTabs.Trigger.Label>Appliance</NativeTabs.Trigger.Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="report">
-        <NativeTabs.Trigger.Icon sf="paperplane.fill" md="explore" />
+        <NativeTabs.Trigger.Icon sf="chart.bar" md="assessment" />
         <NativeTabs.Trigger.Label>Report</NativeTabs.Trigger.Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="scanner-test">
